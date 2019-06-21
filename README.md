f08ef7d9100557ce51a0a341edf2435f98ec3a0b
Angela Laub <angela.laub@gmx.de>
2019-06-21 09:25:29 +0200

    add multiple cats

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-## Project Requirements for Cat Clicker
+## New Project Requirements for Cat Clicker
 
 ## Visuals
-The application should display a picture of a cat and a number of clicks.
+The application should display two cats. Each cat includes
+the cat's name
+a picture of the cat
+text showing the number of clicks
 The specifics of the layout do not matter, so style it however you'd like.
 
 ## Interaction
-The number of clicks should increment when the cat picture is clicked.
+The number of clicks should increment when each cat picture is clicked.
--- js/catclicker.js
+++ js/catclicker.js
@@ -1,13 +1,61 @@
-let catImage = document.getElementById('cat-image');
-let counter = document.getElementById('counter');
+let catField = document.querySelector('.cat-field');
+
+
 let numOfClicks = 0;
 let catName = ['Lucy', 'Miranda'];
 
-catImage.addEventListener('click', function(){
-    console.log("you clicked");
-    numOfClicks++
-    counter.innerHTML = ("Clicks: "+ numOfClicks);
-});
+
+// get the cats onto the page
+let myCatFrag = document.createDocumentFragment();
+
+for(i = 0; i < 2; i++){
+
+    let newCat = document.createElement('div');
+    newCat.classList.add('cat');
+
+    let newCatName = document.createElement('div');
+    newCatName.classList.add('cat-name');
+    newCatName.innerHTML = 'Name: '+catName[i];
+
+    let newCatCounter = document.createElement('div');
+    newCatCounter.classList.add('cat-counter');
+    newCatCounter.innerHTML = 'Clicks: 0';
+
+    let newCatImage = document.createElement('div');
+    newCatImage.classList.add('cat-image');
+    newCatImage.innerHTML = '<img src="img/minimalist-cat-drawing_s.jpg"></img>';
+
+    newCat.appendChild(newCatName);
+    newCat.appendChild(newCatCounter);
+    newCat.appendChild(newCatImage);
+
+
+    myCatFrag.append(newCat);
+
+}
+
+catField.appendChild(myCatFrag);
+
+
+
+let cat = document.querySelector('.cat');
+let catImage = document.querySelector('.cat-image');
+// console.log(catImage);
+let catNameField = document.querySelector('.cat-name');
+// console.log(catNameField);
+let counter = document.querySelector('.cat-counter');
+console.log(counter);
+
+
+
+
+        // TODO: add eventListener to catField
+        // increase number for counter of cat that was clicked
+        cat.addEventListener('click', function(){
+            console.log("you clicked");
+            numOfClicks++
+            counter.innerHTML = ("Clicks: "+ numOfClicks);
+        });
 
 
 
