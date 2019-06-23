8e2fb84a9f266c083db44de08a231dc93070655c
Angela Laub <angela.laub@gmx.de>
2019-06-23 19:54:43 +0200

    separate concerns - my first attempt

--- js/catclicker.js
+++ js/catclicker.js
@@ -1,65 +1,87 @@
-let catField = document.querySelector('.cat-field');
+// Model
+
+let catObject = {
+
+    names: [
+        'Lucy',
+        'Miranda',
+        'Mickey',
+        'Shay',
+        'Smoothie',
+        'Grumpy',
+        'Two-tone',
+        'Cuddly'
+    ],
+
+    images: [
+        'cat.jpg',
+        'cutecat1.webp',
+        'cutecat2n.webp',
+        'kitten-440379.jpg',
+        'Smoothie-the-Cat-Instagram-zoom.jpg',
+        'Tech-In-Two-Grumpy-Cat-453408502-w.jpg',
+        'two-tone-cat.jpg',
+        'unnamed.jpg'
+    ],
+
+    numOfClicks: [0, 0, 0, 0, 0, 0, 0, 0]
+
+};
+
+// View
+
+// Cat list
+let catList = document.querySelector('.cat-list');
+
+// Display Area
 let displayArea = document.querySelector('.display-area');
-let cat = document.querySelector('.cat');
-let catImage = document.querySelector('.cat-image');
-let catName = document.querySelector('.top-cat-name');
-let counter = document.querySelector('.click-counter');
+let displayCatImage = document.querySelector('.display-cat-image');
+let displayCatName = document.querySelector('.display-cat-name');
+let displayCounter = document.querySelector('.display-click-counter');
 
 
-let catNames = [
-    'Lucy',
-    'Miranda',
-    'Mickey',
-    'Shay',
-    'Smoothie',
-    'Grumpy',
-    'Two-tone',
-    'Cuddly'];
+
+// "Octopus?"
+
+function showCatName(index) {
+    displayCatName.innerHTML = "Name: " +catObject.names[index];
+}
+
+// put this as a method in the cat object?
+function increaseCounter(index) {
+    catObject.numOfClicks[index] +=1;
+    displayCounter.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;Clicks: "+catObject.numOfClicks[index];
+};
+
+function showImage(index) {
+    let image = catObject.images[index];
+    displayCatImage.innerHTML = '<img width="400px" src="img/' + image + '">';
+}
 
 
-let catImages = [
-    'cat.jpg',
-    'cutecat1.webp',
-    'cutecat2n.webp',
-    'kitten-440379.jpg',
-    'Smoothie-the-Cat-Instagram-zoom.jpg',
-    'Tech-In-Two-Grumpy-Cat-453408502-w.jpg',
-    'two-tone-cat.jpg',
-    'unnamed.jpg'];
 
+// View
 
-let numOfClicks = [0, 0, 0, 0, 0, 0, 0, 0];
+// show list of cat names
+let catListFrag = document.createDocumentFragment();
 
-
-let myCatFrag = document.createDocumentFragment();
-
-
-for(i = 0; i < catNames.length; i++){
+for(i = 0; i < catObject.names.length; i++){
     let newCatName = document.createElement('div');
     newCatName.classList.add('cat-name');
-    newCatName.innerHTML = catNames[i];
+    newCatName.innerHTML = catObject.names[i];
 
+    // add event listeners to each cat (index saved in a closure)
     newCatName.addEventListener('click', (function(icopy) {
 
-        // without the inner function code gets executed right away for all i's - why?
         return function() {
 
-            catName.innerHTML = "Name: " +catNames[icopy];
-
-            numOfClicks[icopy] +=1;
-            counter.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;Clicks: "+numOfClicks[icopy];
-
-            let image = catImages[icopy];
-            catImage.innerHTML = '<img width="400px" src="img/' + image + '">';
+            showCatName(icopy);
+            increaseCounter(icopy);
+            showImage(icopy);
 
         };
     })(i));
 
-
-    myCatFrag.append(newCatName);
-
+    catListFrag.append(newCatName);
 }
-
-
-catField.appendChild(myCatFrag);
-
+catList.appendChild(catListFrag);
